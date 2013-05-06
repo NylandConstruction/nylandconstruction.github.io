f617a50530472de9b3fabe98a49a2801a16ef9c9
Joe Nyland <joenyland@me.com>
2013-05-06 15:36:03 +0000

    Added Google Analytics tracking code to Nyland Construction site pages.

--- gallery.php
+++ gallery.php
@@ -15,6 +15,19 @@
 	<script type="text/javascript" src="assets/colorbox.js"></script>
 	<script type="text/javascript">$(document).ready(function(){ $("a[rel='cb1']").colorbox(); });</script>
 
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-38684067-1']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+        </script>
 
 
 </head>
--- index.php
+++ index.php
@@ -10,6 +10,19 @@
 	<link rel="icon" href="assets/icon.ico" />
 	<link rel="stylesheet" type="text/css" href="style.css" />
 
+        <script type="text/javascript">
+
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-38684067-1']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+
+        </script>
 
 
 </head>
