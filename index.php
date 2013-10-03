a3734eb99c26bd697fac789c6b49a388b27b7331
Joe Nyland <joenyland@me.com>
2013-10-03 22:18:36 +0000

    Work on ticket #239. Added Velux Installer Partnership logo to partner logo sprites.

--- index.php
+++ index.php
@@ -97,6 +97,7 @@
 			<a href="http://www.niceic.com/" target="_blank"><img class="niceic" src="assets/img_trans.gif" alt="NIC EIC" /></a>
 			<a href="http://www.gassaferegister.co.uk/" target="_blank"><img class="gassafe" src="assets/img_trans.gif" alt="Gas Safe" /></a>
 			<a href="http://www.velux.co.uk/" target="_blank"><img class="velux" src="assets/img_trans.gif" alt="Velux" /></a>
+			<a href="http://www.vipinstallers.com/" target="_blank"><img class="velux-installer-partnership" src="assets/img_trans.gif" alt="Velux Installer Partnership" /></a>
 		</div>
 
 	</div>
