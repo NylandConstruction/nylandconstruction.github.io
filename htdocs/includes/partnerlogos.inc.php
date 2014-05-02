f363f3306713d7d6a5eedf396fad4c51c8bb8731
Joe Nyland <joenyland@me.com>
2014-05-02 12:35:01 +0100

    Moved images to assets/img/

--- htdocs/includes/partnerlogos.inc.php
+++ htdocs/includes/partnerlogos.inc.php
@@ -1,7 +1,7 @@
 <div id="right">
-	<a href="http://www.fmb.org.uk/" target="_blank"><img class="fmb" src="assets/img_trans.gif" alt="Federation of Master Builders" /></a>
-	<a href="http://www.niceic.com/" target="_blank"><img class="niceic" src="assets/img_trans.gif" alt="NIC EIC" /></a>
-	<a href="http://www.gassaferegister.co.uk/" target="_blank"><img class="gassafe" src="assets/img_trans.gif" alt="Gas Safe" /></a>
-	<a href="http://www.velux.co.uk/" target="_blank"><img class="velux" src="assets/img_trans.gif" alt="Velux" /></a>
-	<a href="http://www.vipinstallers.com/" target="_blank"><img class="velux-installer-partnership" src="assets/img_trans.gif" alt="Velux Installer Partnership" /></a>
+	<a href="http://www.fmb.org.uk/" target="_blank"><img class="fmb" src="../assets/img/img_trans.gif" alt="Federation of Master Builders" /></a>
+	<a href="http://www.niceic.com/" target="_blank"><img class="niceic" src="../assets/img/img_trans.gif" alt="NIC EIC" /></a>
+	<a href="http://www.gassaferegister.co.uk/" target="_blank"><img class="gassafe" src="../assets/img/img_trans.gif" alt="Gas Safe" /></a>
+	<a href="http://www.velux.co.uk/" target="_blank"><img class="velux" src="../assets/img/img_trans.gif" alt="Velux" /></a>
+	<a href="http://www.vipinstallers.com/" target="_blank"><img class="velux-installer-partnership" src="../assets/img/img_trans.gif" alt="Velux Installer Partnership" /></a>
 </div>
