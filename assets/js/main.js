7fe994fda9027e75d1bfe45ce6b61c9cdeafb35b
Joe Nyland <joenyland@me.com>
2014-07-08 14:19:55 +0100

    Change index.php to be index.html and remove PHP code

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+/**
+ * Project: www.nylandconstruction.co.uk
+ * File:
+ * User: joenyland
+ * Date: 08/07/2014
+ * Time: 14:16
+ */
+
+function getYear() {
+    var d = new Date();
+    var n = d.getFullYear();
+    document.write(n.toString());
+}
